ad50fad1b40eff37086c3ac3003af848a5925cd1
SYW <2186945919@qq.com>
2024-01-27 21:56:18 +0800

    push commit

--- gitTest.java
+++ gitTest.java
@@ -3,5 +3,6 @@ public class gitTest {
         System.out.println("测试成功01");
         System.out.println("测试成功02");
         System.out.println("测试成功03");
+        System.out.println("push test");
     }
 }
